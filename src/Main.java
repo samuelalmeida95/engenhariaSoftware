7da4da314dca7b32199ea6603cce9172bf3e44be
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2022-05-24 15:10:47 -0300

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
 
+    public static void main(String[] args) {
+
+        SamuelAlmeida somaDeNumeros = new SamuelAlmeida();
+       somaDeNumeros.soma();
+    }
 }
---
+++ src/SamuelAlmeida.java
@@ -0,0 +1,10 @@
+public class SamuelAlmeida {
+
+    int atributo1 = 100;
+    int atributo2 = 200;
+    int atributo3 = 300;
+
+    int soma() {
+        return atributo3 = this.atributo1 + this.atributo2;
+    }
+}
